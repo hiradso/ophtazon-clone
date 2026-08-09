12adac045de2f2908499b80e293a334d8528c62c
hiradso <hiradso@gmail.com>
2026-08-09 20:46:29 +0330

    Debug all route parameters

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -55,7 +55,7 @@ class ProductController extends Controller
 
     public function show(string $product): Response
     {
-        \Illuminate\Support\Facades\Log::info('DEBUG product show received:', ['product' => $product]);
+        \Illuminate\Support\Facades\Log::info('DEBUG all route params:', request()->route()->parameters());
         $product = Product::where('slug', $product)->firstOrFail();
 
         abort_unless($product->status === ProductStatus::Available, 404);
